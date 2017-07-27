20b723877c527ea4243fa2ee84921afe3e347859
sadooghi <gh.sadooghi@gmail.com>
2017-07-27 12:08:31 -0400

    able to delete a post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state={}, action) {
   switch(action.type) {
+    case DELETE_POST:
+      //next line says: if the state object has a key of post id (action.payload), drop it => return a new
+      //object which doesn't have that post
+      return _.omit(state, action.payload);
     case FETCH_POST:
       //const post = action.payload.data;
       //const newState = { ...state };
